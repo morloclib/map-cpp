1756e552dd9b01cb2548eeb16b98c041a2729d22
Zebulun Arendsee <zbwrnz@gmail.com>
2026-07-11 07:44:26 -0400

    Transpose Map wire form to list of tuples

--- map.hpp
+++ map.hpp
@@ -13,26 +13,22 @@
 // --- Pack/Unpack ---
 
 template <class A, class B>
-std::map<A,B> morloc_packMap(std::tuple<std::vector<A>,std::vector<B>> items){
+std::map<A,B> morloc_packMap(std::vector<std::tuple<A,B>> items){
     std::map<A,B> m;
-    std::vector<A> a = std::get<0>(items);
-    std::vector<B> b = std::get<1>(items);
-    assert(a.size() == b.size());
-    for(std::size_t i = 0; i < a.size(); i++){
-        m[a[i]] = b[i];
+    for (const auto& t : items) {
+        m[std::get<0>(t)] = std::get<1>(t);
     }
     return m;
 }
 
 template <class A, class B>
-std::tuple<std::vector<A>,std::vector<B>> morloc_unpackMap(std::map<A,B> m){
-    std::vector<A> a;
-    std::vector<B> b;
-    for (auto tuple : m) {
-        a.push_back(std::get<0>(tuple));
-        b.push_back(std::get<1>(tuple));
+std::vector<std::tuple<A,B>> morloc_unpackMap(std::map<A,B> m){
+    std::vector<std::tuple<A,B>> result;
+    result.reserve(m.size());
+    for (const auto& pair : m) {
+        result.emplace_back(pair.first, pair.second);
     }
-    return std::make_tuple(a, b);
+    return result;
 }
 
 // --- Construction ---
